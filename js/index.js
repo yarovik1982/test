a5f021230c58ff3e3764fe6109b6111e9b6957fa
acunamatata82 <yar.oleg1982@mail.ru>
2024-02-09 23:07:41 +0300

    vesion 1.0.1

--- js/index.js
+++ js/index.js
@@ -1,18 +1,40 @@
 const burgerMenu = () => {
-   console.log("work");
-   const burger = document.querySelector('.header_burger')
+  const burger = document.querySelector(".header_burger");
 
-   const toggleMenu = () => {
-      const header = document.querySelector('.header')
-      const headerHeight = window.getComputedStyle(header).height
-      console.log(headerHeight);
-      const menu = document.querySelector('.header_menu')
-      menu.classList.toggle('show')
-      menu.style.paddingTop = headerHeight
-   }
+  const toggleMenu = () => {
+    const header = document.querySelector(".header");
+    const headerHeight = window.getComputedStyle(header).height;
+    const menu = document.querySelector(".header_menu");
+    menu.classList.toggle("show");
+    menu.style.paddingTop = headerHeight;
+  };
 
-   burger.addEventListener('click', toggleMenu)
-   
-   
+  burger.addEventListener("click", toggleMenu);
+};
+burgerMenu();
+
+const toggleTypeInput = () => {
+  const checkboxes = document.querySelectorAll('.inpCheckTogglerPass');
+  if (!checkboxes || checkboxes.length === 0) {
+    return;
+  } else {
+    console.log(checkboxes);
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', function(e) {
+        const parent = e.target.closest('.form_row');
+        console.log(parent);
+        const input = parent.querySelector('.form_input');
+        console.log(input);
+        if (e.target.checked) {
+          input.setAttribute('type', 'text');
+        } else {
+          input.setAttribute('type', 'password');
+        }
+      });
+    });
+  }
 }
-burgerMenu()
+toggleTypeInput();
+
+
+
---
+++ test.js
@@ -0,0 +1,18 @@
+// console.log(" ");
+// let count = 23;
+// let flag = 10;
+// let space = '  '
+// for (let i = 0; i < 10; i++) {
+//   if (flag > 0) {
+//      flag--
+//    }
+//    let line = space.repeat(flag) + (i+1)+"";
+//   for (let k = 0; k < count + i; k++) {
+//      line += " *  ";
+//   }
+//   console.log( line + (i+1));
+// }
+console.log(+'1' + +'1');
+console.log(parseInt('1') + parseInt('1'));
+console.log("1" + 1);
+console.log("1" + "1");
